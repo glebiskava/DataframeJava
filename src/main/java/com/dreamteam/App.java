6a7568149510aa7d65374605d54231ba86859e4a
glebiskava <leopold.mittelberger@gmail.com>
2025-03-19 09:17:23 +0100

    initial commit

---
+++ src/main/java/com/dreamteam/App.java
@@ -0,0 +1,13 @@
+package com.dreamteam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
